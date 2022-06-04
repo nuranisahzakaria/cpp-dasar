f0cf0059d19bf13dadc7b181486831bb756fac35
nessaaa <nnuranisahh@gmail.com>
2022-06-04 19:23:39 +0700

    ga ngerti lagi, pusing sama cpp

---
+++ 1. variabel.cpp
@@ -0,0 +1,21 @@
+		// DASAR-DASAR C++
+
+// import library
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main(){
+//	inisialisasi
+	float fahr, celcius;
+	
+//	mendapatkan input
+	fahr = 50;
+	
+// proses data
+	celcius = (fahr - 32) * 5/9;
+	
+// 	menampilkan hasil
+	cout << "Hasil konversi = " << celcius << endl;
+	
+}
---
+++ 2. variabel cara 2.cpp
@@ -0,0 +1,18 @@
+//	DASAR-DASAR C++ 02
+
+// include library
+#include<iostream>
+#include<conio.h>
+
+// deklarasi variabel cara 2
+#define PI 3.14
+
+using namespace std;
+
+//operasi matematika
+int _tmain(int argc, _TCHAR* argv[]){
+	float A = asin(0.5);
+	cout << "A sin (0.5) = " << A * 180/PI << endl << endl;
+	_getch();
+	return 0;
+}
